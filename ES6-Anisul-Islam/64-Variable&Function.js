eb08b99af30e7703d146ea1793a73d81cd88de36
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-12 12:57:06 +0600

    added string concatenate

--- ES6-Anisul-Islam/64-Variable&Function.js
+++ ES6-Anisul-Islam/64-Variable&Function.js
@@ -8,10 +8,32 @@ console.log('Bahirer X =',x)
 
 const noChange = 125;
 
-// string concatenate 
+// string concatenate ==>
 
 let a = 25;
 let b = 35;
 
 let sum = a+b
 console.log(`sum is ${sum}. the end`)
+
+let name = 'Abdullah Al Rafi'
+let message = `I am ${name}. I am learning JS`
+console.log(message)
+
+// Function ==> 
+	function add (a, b) {
+		let sum = a+b
+			return sum
+		// console.log(sum)
+	}
+	const added = add(25, 35)
+	// console.log(added)
+
+	// Function in ES6
+
+	const add2 = (a,b) => {
+		let sum = a+b
+		console.log(sum)
+	}
+
+	add2(10,20)
