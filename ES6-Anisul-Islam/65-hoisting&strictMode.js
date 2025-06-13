4d411137f74b2dd6d9c8c4057d56aa58af36726d
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-14 01:53:02 +0600

    added hoisting and strict mode

---
+++ ES6-Anisul-Islam/65-hoisting&strictMode.js
@@ -0,0 +1,13 @@
+// x = 20;
+// console.log(x)
+// var x
+
+// Hoisting and Strict Mode
+
+//pore initialized korleo age niye jabe eitai hoisting mode
+
+// Strick mode code k aro secure and variable dclr kora hoisa kina chk kore
+
+"use strict"
+x = 30
+console.log(x)
