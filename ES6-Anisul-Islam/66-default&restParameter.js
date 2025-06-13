0fb7fe03ac3cd811b283dbda86918469f8dc2c46
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-14 02:05:57 +0600

    added default and rest parameter

---
+++ ES6-Anisul-Islam/66-default&restParameter.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// Default Parameter
+function message(text='value deynai tai eita default parameter'){
+	console.log(text)
+}
+// message('');
+message('text diyechi');
+
+function sum (x, y){
+	let add = x + y
+	console.log(add)
+}
+sum(20, 30)
+
+
+// Rest Parameter
+
+function sum1 (x, y, ...z){
+	console.log(`x = ${x}, y = ${y}, z= ${z}`)
+}
+
+sum1(10, 20, 30 , 40, 50, 60, 70)
