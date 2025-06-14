aee9d7c37bb75147933f5ccf779050f83135e622
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-14 22:27:44 +0600

    added arrow function

---
+++ ES6-Anisul-Islam/72-arrowFunction.js
@@ -0,0 +1,12 @@
+function normalFunction(num) {
+  return num;
+}
+let normalFunctionCall = normalFunction("Hi i am normal function");
+console.log(normalFunctionCall);
+
+// Arrow Function
+
+const arrowFuntion = (num) => {
+  return num;
+};
+console.log(arrowFuntion('Hi i am arrow function'))
