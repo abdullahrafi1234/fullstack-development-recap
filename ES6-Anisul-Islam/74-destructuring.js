e87f9700e3d10bb074897fc0086fa77d68f3a739
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-15 01:30:07 +0600

    added destructuring

---
+++ ES6-Anisul-Islam/74-destructuring.js
@@ -0,0 +1,38 @@
+// Destructuring array and objects
+
+// array destructure
+let numbers = [20, 30, 40, 44];
+let [num1, num2, num3, num4] = numbers;
+
+// console.log(num1);
+// console.log(num2);
+
+// swap variables (just value change)
+let a = 10,  b = 20;
+[a,b] = [b, a]
+
+// console.log(a, b)
+
+//Object Destructure
+const studentInfo = {
+	name: 'rafi',
+	id: 1025,
+	gpa: 2.50
+}
+const {name, id, gpa} = studentInfo
+
+// console.log(id)
+//Nested Object Destructure
+const studentInfoNested = {
+	fullname: 'rafi-bhuiyan',
+	ids: 2025,
+	cgpa: 3.50,
+	laguage : {
+		native: 'bangla',
+		comfortable : 'english'
+	}
+}
+const {fullname,  ids, laguage} = studentInfoNested
+
+console.log(id, laguage.native)
+
