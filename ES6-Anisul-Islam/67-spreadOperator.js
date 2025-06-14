dfb4b5019bad48b0dfc5eb118bf4ee5619a9128d
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-14 12:30:22 +0600

    added sprad operator and use it on array and object concatenate

--- ES6-Anisul-Islam/67-spreadOperator.js
+++ ES6-Anisul-Islam/67-spreadOperator.js
@@ -1,1 +1,35 @@
- 
+ function addNumbers(x, y, z){
+	return x+y+z
+ }
+
+ let numbers = [1, 3, 5]
+//  let checkFunction = addNumbers(numbers[0], numbers[1], numbers[0])
+//  console.log(checkFunction)
+
+console.log(addNumbers(...numbers))  // rest parameter and spread operator are almost same (rest parameter e shodo last variable er jonno use kora jabe)
+
+
+let number1 = [4, 8]
+let number2 = [5, 9]
+
+let numberConcate = number1.concat(number2)
+console.log(numberConcate)
+
+// with spread operator 
+let numbersConcate = [...number1, ...number2]
+console.log(numbersConcate)
+
+
+// Object Concatenate with spread operator 
+let p1 = {
+	name: 'rafi',
+	age: 20
+}
+
+let p2 = {
+	nationality: 'bangladeshi',
+	occupation: 'student'
+}
+
+let p = {...p1, ...p2}
+console.log(p)
