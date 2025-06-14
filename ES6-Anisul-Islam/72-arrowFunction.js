39e126f73dc33b305dfbbdadf7ce0ee5be36bda6
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-14 22:45:04 +0600

    added arrow function

--- ES6-Anisul-Islam/72-arrowFunction.js
+++ ES6-Anisul-Islam/72-arrowFunction.js
@@ -10,3 +10,17 @@ const arrowFuntion = (num) => {
   return num;
 };
 console.log(arrowFuntion('Hi i am arrow function'))
+
+// easy way
+const display = () => console.log('easy arrow function')
+display()
+
+
+function message1(){
+	return 'i am message1'
+}
+console.log(message1())
+
+//
+const message2 = ()=> 'i am message 2'
+console.log(message2())
