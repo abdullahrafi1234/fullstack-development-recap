dbcded5cff4d12243f97422eb0a81cf056c167b6
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-15 20:02:11 +0600

    added destructuring

---
+++ ES6-Anisul-Islam/75-arrayMethods.js
@@ -0,0 +1,4 @@
+// find (callback, value) return the value of the first element that pass certain condition
+
+
+// findIndex (callback, value) return the index of the first element that pass certain condition
