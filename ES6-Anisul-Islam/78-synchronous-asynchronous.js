016b65527e3af5415560dd77cc78da1b43778db5
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-17 01:12:08 +0600

    added synchronous and asynchronous practice

--- ES6-Anisul-Islam/78-synchronous-asynchronous.js
+++ ES6-Anisul-Islam/78-synchronous-asynchronous.js
@@ -1,1 +1,28 @@
- 
+// synchronous programming ( blocking code behavior)
+
+const taskOne = () => {
+  console.log("task1");
+};
+
+const dataLoading = () => {
+	console.log('Data Loading task 2')
+}
+
+const taskTwo = () => {
+  setTimeout(dataLoading,2000)
+};
+const taskThree = () => {
+  console.log("task3");
+};
+const taskFour = () => {
+  console.log("task4");
+};
+const taskFive = () => {
+  console.log("task5");
+};
+
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
+taskFive();
