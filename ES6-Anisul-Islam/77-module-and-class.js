d2410172ad918f28f3fe50030958c3d3ad6cb619
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-16 21:40:59 +0600

    added index file

---
+++ ES6-Anisul-Islam/77-module-and-class.js
@@ -0,0 +1,5 @@
+export let text = 'Welcome to the module'
+
+export function setText(txt){
+	text = txt
+}
---
+++ ES6-Anisul-Islam/index.js
@@ -0,0 +1,4 @@
+import { setText, text } from "./77-module-and-class.js";
+console.log(text)
+setText('goodbye')
+console.log(text)
