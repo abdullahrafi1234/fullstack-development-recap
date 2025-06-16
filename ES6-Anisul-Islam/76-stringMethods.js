5ea183c41829760d931ba5d2f97b825d6fc03e90
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-16 19:38:38 +0600

    added string method, there is three case, startsWith,endsWith,includes

--- ES6-Anisul-Islam/76-stringMethods.js
+++ ES6-Anisul-Islam/76-stringMethods.js
@@ -1,3 +1,16 @@
-let stringMethod = [
-	
-]
+// all these methods are case sensitive and ans in boolean value
+
+// starWith (searchString, position) ==> check a string starts with another string
+
+const message = 'Today is friday'
+console.log(message.startsWith('Today'))
+
+// endsWith (searchString, length) ==> check a string ends with another string
+
+const msg = 'Today is Thursday'
+console.log(msg.endsWith('Thursday'))
+
+// includes (searchString) ==> check if a string contains another string
+
+const msgIncludes = 'Today is Thursday'
+console.log(msg.includes('thursday'))
