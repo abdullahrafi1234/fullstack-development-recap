67e0f45359cc623a77b0735566658417180b0603
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-16 22:00:58 +0600

    added class and constructur,, set and get method

--- ES6-Anisul-Islam/77-module-and-class.js
+++ ES6-Anisul-Islam/77-module-and-class.js
@@ -14,9 +14,26 @@ class Student {
 		this.name = name;
 	}
 
+	// set method
+	set studentName(name){
+		this.name = name;
+	}
+
+	// get method
+	get studentInfo(){
+		return this.id + ' ' + this.name
+	}
+
 }
 
 let s1 = new Student(101, 'Rafi')
 console.log(s1)
 console.log(s1.id)
 console.log(s1.name)
+
+//set clg
+s1.studentName= ('set name Rafi')
+console.log(s1.name)
+
+// get clg 
+console.log(s1.studentInfo)
