d0d34b975149935dbefaaf71ee52664e715a85fe
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-24 18:20:59 +0600

    added local storage and its use

--- ES6-Anisul-Islam/46-webStorage-localStorage-sessionStorage.js
+++ ES6-Anisul-Islam/46-webStorage-localStorage-sessionStorage.js
@@ -6,5 +6,32 @@
 
 // no expiry date : data never gets lost even if you close the browser
 
+// localStorage store data as key value pair ==> string
 
+//setItem (key, value)
+// localStorage.setItem('userName', 'Rafi')
+// localStorage.setItem('password', '258258')
+// console.log(localStorage)
+
+// getItem(key)
+const userName = localStorage.getItem('userName')
+const password = localStorage.getItem('password')
+// console.log(userName, password)
+
+// update ==. neew data boshay dilei hbe
+localStorage.setItem('userName', 'bhuiyan')
+localStorage.setItem('password', '147147')
+// console.log(localStorage)
+
+// remove data
+localStorage.removeItem('userName')
+localStorage.removeItem('password')
+
+// array set on local storage
+const countries = ['Aus', 'Ind', 'Ban']
+localStorage.setItem('countries', JSON.stringify(countries))
+
+//
+const countriesList = JSON.parse(localStorage.getItem('countries'))
+console.log(countriesList)
 
