f0fe28bfcba109b1a102796991922366c7dd6d57
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-24 18:35:21 +0600

    added session storage

--- ES6-Anisul-Islam/46-localStorage.js
+++ ES6-Anisul-Islam/46-localStorage.js
@@ -33,5 +33,18 @@ localStorage.setItem('countries', JSON.stringify(countries))
 
 //
 const countriesList = JSON.parse(localStorage.getItem('countries'))
-console.log(countriesList)
+// console.log(countriesList)
 
+
+// localStorage vs sessionStorage 
+// 10 mb vs 5mb
+// permanent vs session (before tab)
+
+sessionStorage.setItem('user','rafi')
+sessionStorage.setItem('user2','bhuiyan')
+
+// const getName = sessionStorage.getItem('user')
+// console.log(getName)
+
+sessionStorage.removeItem('user')
+sessionStorage.clear()
---
+++ ES6-Anisul-Islam/46-sessionStorage.js
@@ -0,0 +1,12 @@
+// localStorage vs sessionStorage 
+// 10 mb vs 5mb
+// permanent vs session (before tab)
+
+sessionStorage.setItem('user','rafi')
+sessionStorage.setItem('user2','bhuiyan')
+
+// const getName = sessionStorage.getItem('user')
+// console.log(getName)
+
+sessionStorage.removeItem('user')
+sessionStorage.clear()
