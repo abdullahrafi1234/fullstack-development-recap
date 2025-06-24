ef2cbd056ad0ec8790a06a460b391993a4ce4b30
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-24 12:53:27 +0600

    added local storage and session storage file

---
+++ ES6-Anisul-Islam/46-webStorage-localStorage-sessionStorage.js
@@ -0,0 +1,10 @@
+// Web storage API ==> allows us to and read data in browser
+
+// Web API ==> local storage , session storage
+
+// local storage ==> store, read, update and remove data
+
+// no expiry date : data never gets lost even if you close the browser
+
+
+
