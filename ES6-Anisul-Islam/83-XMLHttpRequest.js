6fe34e163b630ccaae6d7028284be605451a10f0
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-06-29 19:06:10 +0600

    added XMLHttpRequest

--- ES6-Anisul-Islam/83-XMLHttpRequest.js
+++ ES6-Anisul-Islam/83-XMLHttpRequest.js
@@ -1,10 +1,68 @@
 console.clear()
 // console.log(window)
 
-const getData = () => {
-	const xhr = new XMLHttpRequest()
 
-	console.log(xhr)
+// event ==> onload(), onerror() 
+// property ==> response, responseText, responseURL, status , statusText
+// function ==> open() , send() , setRequestHeader()
+
+
+const makeRequest = (method, url, data) => {
+
+const xhr = new XMLHttpRequest()
+
+	xhr.open(method, url)
+
+	xhr.setRequestHeader('Content-Type', 'application/json')
+
+	xhr.onload = () => {
+		let data = xhr.response
+		// console.log(data)
+		console.log(JSON.parse(data))
+	}
+
+	xhr.onerror= () => {
+		console.log('error is here')
+	}
+
+	xhr.send(JSON.stringify(data))
+
 }
 
-getData()
+
+const getData = () => {
+	makeRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+	// console.log(xhr)\
+}
+
+const sendData = () => {
+	makeRequest('POST', 'https://jsonplaceholder.typicode.com/posts', {
+		title: 'foo',
+    	body: 'bar',
+   		userId: 1,
+	})
+}
+
+const updateData = () => { 
+	makeRequest('PUT', 'https://jsonplaceholder.typicode.com/posts/1', {
+		id: 1,
+		title: 'foo update',
+    	body: 'bar',
+   		userId: 1,
+	})
+}
+const updateSingleData = () => { 
+	makeRequest('PATCH', 'https://jsonplaceholder.typicode.com/posts/1', {
+		title: 'foo single update',
+	})
+}
+const deleteData = () => { 
+	makeRequest('DELETE', 'https://jsonplaceholder.typicode.com/posts/1',)
+}
+
+
+// getData()
+// sendData()
+// updateData()
+// updateSingleData()
+deleteData()
