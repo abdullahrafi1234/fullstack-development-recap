89c2f9bdb76f5cec69677957890654101f786b20
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-01 01:01:46 +0600

    added jquery

--- ES6-Anisul-Islam/86-jQuery.js
+++ ES6-Anisul-Islam/86-jQuery.js
@@ -2,3 +2,55 @@
 // ajax => asynchronous js and xml
 
 console.log(window)
+
+const makeRequest =async (url, method, data) => {
+
+	try{
+		const result =await $.ajax({
+		url: url,
+		method: method,
+		data: data
+	})
+	// console.log(result)
+	return result;
+
+	}catch(err){
+		console.log(err)
+	}
+}
+
+// const getData = () => {
+// 	makeRequest('https://jsonplaceholder.typicode.com/posts', "GET")
+// 	.then((res)=> console.log(res))
+// 	// .catch((err) => console.log(err))
+// }
+
+// const createData = () => {
+// 	makeRequest('https://jsonplaceholder.typicode.com/posts', "POST", {
+// 		title: 'foooo',
+// 		body: 'bar',
+// 		userId: 1
+// 	})
+// 	.then((res)=> console.log(res))
+// 	// .catch((err) => console.log(err))
+// }
+const updateData = () => {
+	makeRequest('https://jsonplaceholder.typicode.com/posts/1', "PUT", {
+		title: 'fooooww',
+		body: 'barww',
+		userId: 1,
+		id: 1
+	})
+	.then((res)=> console.log(res))
+	// .catch((err) => console.log(err))
+}
+
+const deleteData = () => {
+	makeRequest('https://jsonplaceholder.typicode.com/posts/1', 'DELETE')
+	.then((res)=> console.log(res))
+}
+
+// getData()
+// createData()
+// updateData()
+deleteData()
