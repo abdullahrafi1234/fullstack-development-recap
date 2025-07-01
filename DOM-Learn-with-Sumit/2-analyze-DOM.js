8795b6c89e2ef945d3869d31b9a3e35f3c8b987e
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-02 02:07:30 +0600

    added analyze dom

--- DOM-Learn-with-Sumit/2-analyze-DOM.js
+++ DOM-Learn-with-Sumit/2-analyze-DOM.js
@@ -1,2 +1,8 @@
 
+// GET ELEMENT BY ID
 
+let headerElement = document.getElementById('header')
+console.log(headerElement.textContent) // text er modde ja ase oita output de
+console.log(headerElement.innerText)  // final output e ja dekhte passe oita de
+
+// console.log(headerElement.innerHTML)
--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+
+
+// # Basic Topics
+// 🔹 Data Types (Primitive & Non-Primitive)
+// 🔹 Type Coercion (== vs ===)
+// 🔹 Scope (var, let, const)
+// 🔹 Strict Mode
+
+// # Control Flow & Array Methods
+// 🔹 if/else, switch এবং ternary
+// 🔹 for, while, for...in, for...of
+// 🔹 .map(), .filter(), .reduce(), .find() etc.
+
+// # Async JavaScript
+// 🔹 setTimeout, setInterval
+// 🔹 Promises, async/await
+// 🔹 Event Loop, Call Stack
+
+// # Functions:
+// 🔹 Function Declaration vs Expression
+// 🔹 Arrow Function vs Normal Function
+// 🔹 Callback, Higher-Order Functions
+
+// # Objects & Arrays Handling
+// 🔹 Destructuring, Spread, Rest
+// 🔹 Object.keys(), Object.values(), Optional Chaining
+
+// # Error Handling
+// 🔹 try/catch
+// 🔹 Custom error handling
+
+// Topics ++
+// 🔹 Debounce, Throttle, Memoization
+// 🔹 DOM Manipulation & Event Delegation
+// 🔹 LocalStorage / SessionStorage
+// 🔹 ES6 Modules (import/export)
+// 🔹 Fetch API & Error Handling
+
+
+
+
 // module related output from
 
 import { setText, text } from "./77-module-and-class.js";
