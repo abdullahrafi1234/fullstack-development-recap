7ca02a4222807b656763809e2227e65ddd4ae117
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-02 02:36:25 +0600

    added analyze dom

--- DOM-Learn-with-Sumit/2-analyze-DOM.js
+++ DOM-Learn-with-Sumit/2-analyze-DOM.js
@@ -2,7 +2,7 @@
 // GET ELEMENT BY ID
 
 let headerElement = document.getElementById('header')
-console.log(headerElement.textContent) // text er modde ja ase oita output de
-console.log(headerElement.innerText)  // final output e ja dekhte passe oita de
+console.log(headerElement.textContent) // shodo matro text er modde ja ase oita output de
+console.log(headerElement.innerText)  // css apply korar por final output e ja dekhte passe oita de
 
-// console.log(headerElement.innerHTML)
+console.log(headerElement.innerHTML)
---
+++ DOM-Learn-with-Sumit/3-traversing.js
@@ -0,0 +1,8 @@
+
+// GET ELEMENTS BY CLASS NAME
+
+let items = document.getElementsByClassName('item')
+
+for(let i=0; i<items.length; i++){
+	items[i].style.color = 'red'
+}
