3195b47694520c3d5bc4595705ecb0eec86a4b2b
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-02 02:54:26 +0600

    practice dom

--- DOM-Learn-with-Sumit/3-traversing.js
+++ DOM-Learn-with-Sumit/3-traversing.js
@@ -1,8 +1,17 @@
 
 // GET ELEMENTS BY CLASS NAME
 
-let items = document.getElementsByClassName('item')
+// let items = document.getElementsByClassName('item')
 
-for(let i=0; i<items.length; i++){
-	items[i].style.color = 'red'
-}
+// for(let i=0; i<items.length; i++){
+// 	items[i].style.color = 'red'
+// }
+
+
+let tagName = document.getElementsByTagName('li')
+// console.log(tagName)
+
+
+// Query Selector
+let task = document.querySelector('#new-task')
+console.log(task)
