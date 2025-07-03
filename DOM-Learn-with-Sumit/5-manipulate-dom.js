7a40b8359e34d87e0ae0323ea3ed5be94ceb723c
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-03 17:36:23 +0600

    practice dom

--- DOM-Learn-with-Sumit/5-manipulate-dom.js
+++ DOM-Learn-with-Sumit/5-manipulate-dom.js
@@ -3,3 +3,17 @@
 const divElement = document.createElement('div')
 
 divElement.className = 'red'
+
+divElement.setAttribute('id', 'red')
+divElement.setAttribute('title', 'Red Div')
+// console.log(divElement)
+
+const container = document.querySelector('.todo-list')
+// const h2Element = container.querySelector
+// ('h2')
+// container.insertBefore(divElement, h2Element)
+
+const a = container.appendChild(divElement)   // append child er modde html element dite hoy
+const b = container.append(divElement, 'Hello World')   /// append er modde normal string deoa jay and chaile 2, 3 ta element ekshate deoa jay
+
+console.log(a, b)
