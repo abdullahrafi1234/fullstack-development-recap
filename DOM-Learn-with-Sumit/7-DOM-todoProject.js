9c5253a18cfe4f6c73faa26c6dabfc89ebf431f8
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-05 01:32:51 +0600

    practice dom

--- DOM-Learn-with-Sumit/7-DOM-todoProject.js
+++ DOM-Learn-with-Sumit/7-DOM-todoProject.js
@@ -11,3 +11,63 @@
 
 let newTask = document.querySelector('#new-task')
 let form = document.querySelector('form')
+let todoUl = document.querySelector('#items')
+let completeUl = document.querySelector('.complete-list ul')
+
+// functions
+let createTask = function(task){
+	let listItem = document.createElement('li')
+	let checkBox = document.createElement('input')
+	let label = document.createElement('label')
+
+	label.innerText = task;
+	checkBox.type = 'checkbox';
+
+	listItem.appendChild(checkBox)
+	listItem.appendChild(label)
+
+	return listItem;
+}
+
+let addTask = function(e){
+	e.preventDefault()
+
+	let listItem = createTask(newTask.value)
+	todoUl.appendChild(listItem);
+	newTask.value = ''
+	// bind the new list to the incomplete list
+	bindInCompleteItems(listItem, completeTask)
+}
+
+let completeTask = function(){
+	let listItem = this.parentNode;
+	let deleteBtn = document.createElement('button');
+	deleteBtn.innerText = 'Delete'
+	deleteBtn.className = 'delete'
+	listItem.appendChild(deleteBtn)
+
+	let checkBox = listItem.querySelector('input[type= "checkbox"]')
+	checkBox.remove();
+	completeUl.appendChild(listItem)
+
+	bindCompleteItems(listItem, deleteTask)
+}
+
+let deleteTask = function(){
+	let listItem = this.parentNode;
+	let ul = listItem.parentNode;
+	ul.removeChild(listItem)
+}
+
+let bindInCompleteItems = function(taskItem, checkBoxClick){
+	let checkBox = taskItem.querySelector('input[type="checkbox"]')
+	checkBox.onchange = checkBoxClick;
+}
+
+
+let bindCompleteItems = function(taskItem, deleteButtonClick){
+	let deleteButton = taskItem.querySelector('.delete')
+	deleteButton.onclick= deleteButtonClick;
+}
+
+form.addEventListener('submit', addTask)
