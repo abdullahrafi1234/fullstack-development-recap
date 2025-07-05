6305ada76f1eadfe9fe60ef271eab541a4aef101
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-05 14:16:26 +0600

    added es6 learn with sumit

--- ES6-Learn-with-Sumit/9-splice.js
+++ ES6-Learn-with-Sumit/9-splice.js
@@ -1,2 +1,8 @@
 // Array.prototype.splice()
 
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+const result = numbers.splice(1,2, 10, 12, 15)
+ // 3 ta parameter nite pare,    1 index e cop dibe then 2 ta delete korbe, then baki gula array te add kore main array k modify kore dibe
+console.log(result) // ber kore dise amn result pabo
+console.log(numbers)
