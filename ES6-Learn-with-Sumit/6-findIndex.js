7ec801c37cb48fbe078f95a5eb3f0b9564332dc3
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-05 13:44:37 +0600

    added es6 learn with sumit

--- ES6-Learn-with-Sumit/6-findIndex.js
+++ ES6-Learn-with-Sumit/6-findIndex.js
@@ -3,7 +3,8 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 let result = numbers.findIndex((currentValue, index, arr) => {
-	return !(currentValue % 2)
+	return (currentValue > 20)
+	// jokhn array te kisu na pay taile (-1) return korbe
 })
 
 console.log(result)
