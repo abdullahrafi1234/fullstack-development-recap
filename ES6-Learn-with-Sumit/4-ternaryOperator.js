0fbccccab3e3490509a716a49f0f6ab793549f39
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-05 13:17:02 +0600

    added es6 learn with sumit

---
+++ ES6-Learn-with-Sumit/4-ternaryOperator.js
@@ -0,0 +1,19 @@
+// ternary operator
+
+let age = 11;
+let type = age>=18? 'adult' : 'child'
+console.log(type)
+
+
+let isLoggedIn = false;
+// let access = isLoggedIn? true: false;
+let access = isLoggedIn;
+console.log(access)
+
+// if(age>= 18){
+// 	type= 'adult'
+// }else{
+// 	type= 'child';
+// }
+
+
