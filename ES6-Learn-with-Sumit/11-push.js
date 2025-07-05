021a2113ca83f84d0b0b93e7181c5e0ea6c55fc3
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-06 01:26:04 +0600

    added es6 learn with sumit

--- ES6-Learn-with-Sumit/11-push.js
+++ ES6-Learn-with-Sumit/11-push.js
@@ -3,7 +3,7 @@
 const numbers = [1, 2, 3, 4]
 const pushArray = [3, 4, 7]
 
-const result = numbers.push(pushArray)
+const result = numbers.push(5, 5, 6, 6)
 console.log(numbers)
 console.log(result)
 
