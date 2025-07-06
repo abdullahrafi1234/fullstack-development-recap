a2e235fcdc88a301f4e0fb1daa2e41ba36ad1ed7
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-06 16:30:32 +0600

    added es6 learn with sumit

--- ES6-Learn-with-Sumit/12-map.js
+++ ES6-Learn-with-Sumit/12-map.js
@@ -1,2 +1,11 @@
 // Array.prototype.map()
 
+// eita for loop method er moto kaj kore and eita ekta function receive kore
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const result = numbers.map(number => {
+	console.log(number)
+})
+
+numbers.map()
