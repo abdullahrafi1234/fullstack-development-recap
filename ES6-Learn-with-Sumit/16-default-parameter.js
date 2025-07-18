2b114b535aeaf1d2ce36c54e11acc85ff8dac7f5
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-19 03:05:06 +0600

    added object

--- ES6-Learn-with-Sumit/16-default-parameter.js
+++ ES6-Learn-with-Sumit/16-default-parameter.js
@@ -1,9 +1,14 @@
 // Function default parameter value
 
-function myFunc (x){
+function myFunc (x=10){
 	return x
 	
 }
-let funct = myFunc(25)
-console.log(funct)
+let funct1 = myFunc(45)  // value dile oitai dekhabe
+let funct2 = myFunc()  //value na dile default ta dekhabe
+let funct3 = myFunc(null) // null dile null dekhabe karon eitao ekta value
+
+console.log(funct1)
+console.log(funct2)
+console.log(funct3)
 
