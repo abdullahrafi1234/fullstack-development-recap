40bd3eee3cc1adea87f4b434c77be6f50876c6b6
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-20 01:20:31 +0600

    added object

--- ES6-Learn-with-Sumit/16-default-parameter.js
+++ ES6-Learn-with-Sumit/16-default-parameter.js
@@ -8,7 +8,7 @@ let funct1 = myFunc(45)  // value dile oitai dekhabe
 let funct2 = myFunc()  //value na dile default ta dekhabe
 let funct3 = myFunc(null) // null dile null dekhabe karon eitao ekta value
 
-console.log(funct1)
-console.log(funct2)
-console.log(funct3)
+// console.log(funct1)
+// console.log(funct2)
+// console.log(funct3)
 
