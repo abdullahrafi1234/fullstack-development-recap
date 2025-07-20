72358fe9009191cc38b7340b9bb539837ad4d090
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-21 01:45:38 +0600

    added object

--- ES6-Learn-with-Sumit/18-restOperator.js
+++ ES6-Learn-with-Sumit/18-restOperator.js
@@ -1,7 +1,23 @@
-// Rest Operator
+// Rest Operator (function er parameter hishebe use hbe)
 
 function myFunc(a, b) {
-	console.log(a,b)
+	// console.log(a,b)
 }
 
 myFunc(4,5)
+
+
+function myFunc2(a, ...params) {
+	console.log(a)
+	console.log(params)
+}
+
+myFunc2(4,5,6, 7, 9)
+
+
+// ager way
+function argumentsPrac () {
+	// console.log(arguments)
+}
+
+argumentsPrac(1, 2, 3, 4)
