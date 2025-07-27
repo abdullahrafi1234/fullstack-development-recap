32406e154c781693a33def1b122c5c1863c899be
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-27 13:23:18 +0600

    tagged template literals

--- ES6-Learn-with-Sumit/22-tagged-templateLiteral.js
+++ ES6-Learn-with-Sumit/22-tagged-templateLiteral.js
@@ -1,4 +1,15 @@
+// tagged template literals 
+// shift()  shodo matro 1st value ta dhore niye ashe
+
+function modifier (strings, ...values){
+	const m = strings.reduce((prevValue, CurrentValue) => {	
+		return prevValue + CurrentValue + (values.length ? 'Mr.' + values.shift() : '')
+	}, "")
+	return m
+}
+
 const player1 = 'Sakib';
 const player2 = 'Tamim';
 
-console.log(`We have ${player1} and ${player2} in our cricket team.`);
+console.log(modifier`We have ${player1} and ${player2} in our cricket team.`);
+
