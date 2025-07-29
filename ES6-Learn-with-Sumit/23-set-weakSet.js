e7b02f7b075885339d38090b8f73ff6ce3de5879
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-07-30 01:46:18 +0600

    set and weakSet

--- ES6-Learn-with-Sumit/23-set-weakSet.js
+++ ES6-Learn-with-Sumit/23-set-weakSet.js
@@ -1,7 +1,28 @@
 // set and weak set 
 
 // Sets
+/** Create set ==> let mySet = new Set();
+ * 1. add()
+ * 2. delete()
+ * 3. has()
+ * 4. clear() 
+ * 5. size()
+ */
 
 // let myArray = new Array();
 let mySet = new Set();
+
+mySet.add(5)
+mySet.add('Bangladesh')
+// mySet.add('Bangladesh') // 2 bar dile 1 bar hoy
+
+mySet.delete('Bangladesh')
+
+// chain kora jay
+mySet.add(10).add('BD').add('Nepal').delete(5)
+// mySet.add(10).add('BD').add('Nepal').clear()   // set full clear hoye jabe
+
+// console.log(mySet.has('Bangladesh'))
+
 console.log(mySet)
+console.log(mySet.size)
