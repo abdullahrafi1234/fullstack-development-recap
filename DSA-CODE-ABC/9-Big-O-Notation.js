a5f2eab582cbd6788681762a4330a47813620972
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-08-05 13:27:14 +0600

    practice dsa

--- DSA-CODE-ABC/9-Big-O-Notation.js
+++ DSA-CODE-ABC/9-Big-O-Notation.js
@@ -4,5 +4,13 @@ const myObject = {
 	videos: [4, 5, 6, 7]
 }
 
-// console.log(Object.keys(myObject)); // big-O(n) {input baraile output barbe tai n} 
-console.log(Object.entries(myObject)); 
+// console.log(Object.keys(myObject)); // big-O(n) {input baraile output barbe tai n}
+
+// console.log(Object.entries(myObject)); 
+
+// console.log(myObject.hasOwnProperty("channel")); // {ekhane output ashbe 1 tai tai eita} big-O(1)
+
+
+
+let myArray = ['hello', 'gello', 'cello', 'dello']   // array te 1st er dike je kono operation korle eitay time complexity bare karon shobar index change hoy... big-O(n)
+
